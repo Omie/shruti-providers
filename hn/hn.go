926817fd1ed332428812664620174eb3b89f8df7
omie <intelomkar@gmail.com>
2015-07-25 04:24:09 +0530

    added hn plugin using Go, easier for deployment

---
+++ hn/hn.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	shruti "github.com/omie/shruti-go"
+	"github.com/peterhellberg/hn"
+)
+
+const (
+	PROVIDER_NAME = "hackernews2"
+)
+
+var (
+	sClient shruti.Client
+	visited map[int]bool
+)
+
+func Register() (err error) {
+
+	p := shruti.Provider{Name: PROVIDER_NAME,
+		DisplayName: "Hacker News",
+		Description: "updates around the globe, mostly tech related",
+		WebURL:      "https://news.ycombinator.com/",
+		IconURL:     "https://news.ycombinator.com/favicon.ico",
+		Voice:       "Emma",
+	}
+
+	err = sClient.RegisterProvider(p)
+
+	return
+
+}
+
+func doWork() (err error) {
+	hn := hn.NewClient(nil)
+
+	for {
+		ids, err := hn.TopStories()
+		if err != nil {
+			return err
+		}
+
+		for _, id := range ids[:10] {
+			item, err := hn.Item(id)
+			if err == nil {
+				n := shruti.Notification{
+					Title:        item.Title,
+					Url:          item.URL,
+					Key:          PROVIDER_NAME + strconv.Itoa(item.ID),
+					Priority:     shruti.PRIO_MED,
+					Action:       shruti.ACT_PUSH,
+					ProviderName: PROVIDER_NAME,
+				}
+				err = sClient.PushNotification(n)
+				msg := "submitted"
+				if err != nil {
+					msg = err.Error()
+				}
+				log.Println(msg)
+			}
+		}
+		log.Println("Sleeping")
+		time.Sleep(15 * time.Minute)
+		log.Println("woke up")
+	}
+}
+
+func main() {
+
+	log.Println("Starting Hacker News Scraper")
+
+	shrutiServer := os.Getenv("SHRUTI_SERVER")
+	if shrutiServer == "" {
+		log.Println("SHRUTI_SERVER not set")
+		return
+	}
+
+	sClient = shruti.Client{"http://", shrutiServer}
+
+	err := Register()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = doWork()
+	if err != nil {
+		log.Println(err)
+	}
+}

e730f54dee21b45f4f4ffbe9c64dc7eb988b9fef
omie <intelomkar@gmail.com>
2015-07-25 17:46:10 +0530

    use main file to help track godeps

---
+++ main.go
@@ -0,0 +1,11 @@
+package shrutiproviders
+
+import (
+	_ "github.com/omie/shruti-providers/bbc"
+	_ "github.com/omie/shruti-providers/hn"
+	_ "github.com/omie/shruti-providers/reddit"
+)
+
+func main() {
+	// dummy file to help with Godeps
+}

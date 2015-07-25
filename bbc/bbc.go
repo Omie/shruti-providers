8ffeb472f69cab534b422a62ccd43e33392ba51d
omie <intelomkar@gmail.com>
2015-07-25 14:53:56 +0530

    bbc provider in Go, easier to deploy (:

---
+++ bbc/bbc.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/SlyMarbo/rss"
+	shruti "github.com/omie/shruti-go"
+)
+
+const (
+	PROVIDER_NAME = "bbc"
+	BBC_FEED_URL  = "http://feeds.bbci.co.uk/news/world/rss.xml?edition=uk"
+)
+
+var (
+	sClient shruti.Client
+	visited map[int]bool
+)
+
+func Register() (err error) {
+
+	p := shruti.Provider{Name: PROVIDER_NAME,
+		DisplayName: "BBC",
+		Description: "world news headlines from BBC",
+		WebURL:      "http://bbc.com",
+		IconURL:     "http://static.bbci.co.uk/frameworks/barlesque/2.83.10/desktop/3.5/img/blq-blocks_grey_alpha.png",
+		Voice:       "Brian",
+	}
+
+	err = sClient.RegisterProvider(p)
+
+	return
+
+}
+
+func doWork() (err error) {
+
+	feed, err := rss.Fetch(BBC_FEED_URL)
+	if err != nil {
+		return err
+	}
+
+	for {
+		for _, item := range feed.Items[:10] {
+			n := shruti.Notification{
+				Title:        item.Title,
+				Url:          item.Link,
+				Key:          PROVIDER_NAME + item.ID,
+				Priority:     shruti.PRIO_MED,
+				Action:       shruti.ACT_PUSH,
+				ProviderName: PROVIDER_NAME,
+			}
+			err = sClient.PushNotification(n)
+			msg := "submitted"
+			if err != nil {
+				msg = err.Error()
+			}
+			log.Println(msg)
+		}
+		log.Println("Sleeping")
+		time.Sleep(15 * time.Minute)
+		log.Println("woke up")
+
+		err = feed.Update()
+		if err != nil {
+			return err
+		}
+
+	}
+}
+
+func main() {
+
+	log.Println("Starting BBC News Scraper")
+
+	shrutiServer := os.Getenv("SHRUTI_SERVER")
+	if shrutiServer == "" {
+		log.Println("SHRUTI_SERVER not set")
+		return
+	}
+
+	sClient = shruti.Client{"http://", shrutiServer}
+
+	err := Register()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = doWork()
+	if err != nil {
+		log.Println(err)
+	}
+}
